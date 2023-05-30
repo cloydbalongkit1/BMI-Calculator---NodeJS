d561d4ee5795528dfa5fe906b9f7ba5076795241
Cloyd <107380041+cloydbalongkit1@users.noreply.github.com>
2023-05-30 14:38:31 +0800

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+app.post("/", (req, res) => {
+    let weight = Number(req.body.number1);
+    let height = Number(req.body.number2);
+    
+    let BMI = Math.floor(weight/(height * height));
+
+    res.send("<h1>Your BMI is: " + BMI + "</h1>");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
